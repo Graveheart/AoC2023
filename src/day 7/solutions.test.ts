b273dbed5969b13d9332ef1e64309e295fd299c4
Venelin Banov <vbanov@dataminr.com>
2023-12-20 10:32:12 +0100

    feat: add solution for day 7 part 1

--- src/day 7/solutions.test.ts
+++ src/day 7/solutions.test.ts
@@ -1,15 +1,16 @@
 import { describe, expect, it } from "bun:test";
-// import { findWinningSum, findWinningWays } from "./solutions";
+import { getTotalWinnings } from "./solutions";
 
 describe("Day 7", async () => {
 	const input = await Bun.file("test.txt").text();
-	// it("Part One", async () => {
-	// 	const res = findWinningSum(input);
-	// 	expect(res).toEqual(288);
-	// });
+	it("Part One", async () => {
+		const res = getTotalWinnings(input);
+		expect(res).toEqual(6440);
+	});
 
-	// it("Part Two", async () => {
-	// 	const res = findWinningWays(input);
-	// 	expect(res).toEqual(71503);
-	// });
+	it("Part One test 2", async () => {
+		const input = await Bun.file("test2.txt").text();
+		const res = getTotalWinnings(input);
+		expect(res).toEqual(18075);
+	});
 });
